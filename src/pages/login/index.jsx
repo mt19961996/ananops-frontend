5c3ff14d9f8b530ab689030e3911dfd71db7cf3e
llr <757493952@qq.com>
2019-12-04 21:35:45 +0800

    diuqi

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -4,22 +4,22 @@ import { withRouter, Redirect, Route } from 'react-router-dom'
 class Login extends Component {
 
 login=()=>{
-localStorage.setItem("login",true)
+  localStorage.setItem("login",true)
 }
 
-    render(){
-        if(localStorage.getItem("login")){
-            return(
-                <Redirect to="/" />
-            )
-        }
-        return(
-            <div>
+render(){
+  if(localStorage.getItem("login")){
+    return(
+      <Redirect to="/" />
+    )
+  }
+  return(
+    <div>
                 login
-                <button onClick={this.login}>
-                </button>
-            </div>
-        )
-    }
+      <button onClick={this.login}>
+      </button>
+    </div>
+  )
+}
 }
 export default withRouter(Login)
--- src/pages/primiaryLayout/index.jsx
+++ src/pages/primiaryLayout/index.jsx
@@ -2,17 +2,17 @@ import React, { Component } from 'react'
 import { withRouter, Redirect, Route } from 'react-router-dom'
 
 class PrimiaryLayout extends Component {
-    render(){
-        if(!localStorage.getItem('login')){
-            return(
-                <Redirect to="/login"/>
-            )
-        }
-        return(
-            <div>
-                primiary
-            </div>
-        )
+  render(){
+    if(!localStorage.getItem('login')){
+      return(
+        <Redirect to="/login"/>
+      )
     }
+    return(
+      <div>
+                primiary
+      </div>
+    )
+  }
 }
 export default withRouter(PrimiaryLayout)
