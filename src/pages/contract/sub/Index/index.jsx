47f636b0c1367a66642fb920f9057625b66e6cfb
Rusty-Trueno <1076061276@qq.com>
2020-01-13 21:35:24 +0800

    实现了甲方负责人审批后，工单自动跳转到服务商待接单阶段

--- src/pages/contract/sub/Index/index.jsx
+++ src/pages/contract/sub/Index/index.jsx
@@ -181,9 +181,9 @@ class Sub extends Component{
             const imcTaskDetail = res2.data.result;
             const imcItemDetail = res3.data.result;
             let createImcInfo = {
-                facilitatorId:projectDetail.partyBId,
-                facilitatorGroupId:projectDetail.partyBId,
-                facilitatorManagerId:projectDetail.partyBId,
+                facilitatorId:projectDetail.bleaderId,
+                facilitatorGroupId:projectDetail.bleaderId,
+                facilitatorManagerId:projectDetail.bleaderId,
                 frequency:imcTaskDetail.cycleTime,
                 inspectionType:1,
                 days:imcTaskDetail.scheduledFinishTime,
--- src/pages/inspection/Inspection/index.jsx
+++ src/pages/inspection/Inspection/index.jsx
@@ -22,6 +22,8 @@ class Inspection extends Component{
             // total: 20, 
             nowCurrent:FIRST_PAGE,
             data:[],
+            display_button1:'none',
+            display_button2:'none',
         }
         this.getInfo=this.getInfo.bind(this)
     }
@@ -31,65 +33,120 @@ class Inspection extends Component{
 
   //根据不同的路由，加载不同的信息
   getInfo(page){  
-    var location=this.props.location.pathname
-    var status
- 
-    if(location == '/cbd/inspection/check'){
-      status=0
+      var location=this.props.location.pathname
+      var status
+  
+      if(location == '/cbd/inspection/check'){
+        status=0
+        if(this.state.role!=null && this.state.role.includes('用户')){
+          this.setState({
+            display_button1:'block',
+            display_button2:'none',
+          })
+        }
+      }
+      if(location=='/cbd/inspection/accept'){  
+        status=2
+        this.setState({
+          display_button1:'none',
+          display_button2:'block',
+        })
+      }
+      else if(location==='/cbd/inspection/execute'){
+        status=3
+        this.setState({
+          display_button1:'none',
+          display_button2:'none',
+        })
+      }
+      else if(location==='/cbd/inspection/confirm'){
+        status=4
+        this.setState({
+          display_button1:'none',
+          display_button2:'none',
+        })
+      }
+      else if(location==='/cbd/inspection/pay'){
+        status=5
+        this.setState({
+          display_button1:'none',
+          display_button2:'none',
+        })
+      }
+      else if(location==='/cbd/inspection/comment'){
+        status=6
+        this.setState({
+          display_button1:'none',
+          display_button2:'none',
+        })
+      }
+      else if(location==='/cbd/inspection/finish'){
+        status=7
+        this.setState({
+          display_button1:'none',
+          display_button2:'none',
+        })
+      }
+      const { size, } = this.state;
+      var whichRole=null
+      const {role}=this.state
+      if(role==='用户管理员'||role==='用户负责人'||role==='用户值机员')
+        whichRole=1;
+      else if(role==='服务商管理员'||role==='服务商负责人'||role==='服务商业务员')
+        whichRole=2 
+      
+      const values={orderBy: "string",pageSize:size,pageNum:page,userId:this.state.id,role:whichRole,status:status,projectId:null}
+      console.log(values)
+      axios({
+          method: 'POST',
+          url: '/imc/inspectionTask/getTaskByUserIdAndStatus',
+          headers: {
+            'deviceId': this.deviceId,
+            'Authorization':'Bearer '+this.state.token,
+          },
+          data:values
+        })
+      .then((res) => {
+          if(res && res.status === 200){
+          this.setState({
+              data: res.data.result,
+              nowCurrent:res.data.result.pageNum,
+              status:status,
+              // roleCode:roleCode,
+          });
+          console.log(res.data)
+          }
+      })
+      .catch(function (error) {
+          console.log(error);
+      });
+        
+  }
+  //同意该巡检任务的执行
+  approveImcTask=(id)=>{
+    console.log("当前巡检任务id为：" + id)
+    const data={
+      taskId:id
     }
-    if(location=='/cbd/inspection/accept'){  
-      status=2
-    }
-    else if(location==='/cbd/inspection/execute'){
-      status=3
-    }
-    else if(location==='/cbd/inspection/confirm'){
-      status=4
-    }
-    else if(location==='/cbd/inspection/pay'){
-      status=5
-    }
-    else if(location==='/cbd/inspection/comment'){
-      status=6
-    }
-    else if(location==='/cbd/inspection/finish'){
-      status=7
-    }
-    const { size, } = this.state;
-    var whichRole=null
-    const {role}=this.state
-    if(role==='用户管理员'||role==='用户负责人'||role==='用户值机员')
-      whichRole=1;
-    else if(role==='服务商管理员'||role==='服务商负责人'||role==='服务商业务员')
-      whichRole=2 
-    
-    const values={orderBy: "string",pageSize:size,pageNum:page,userId:this.state.id,role:whichRole,status:status,projectId:null}
-    console.log(values)
     axios({
         method: 'POST',
-        url: '/imc/inspectionTask/getTaskByUserIdAndStatus',
+        url: '/imc/inspectionTask/acceptImcTaskByPrincipal',
         headers: {
           'deviceId': this.deviceId,
           'Authorization':'Bearer '+this.state.token,
         },
-        data:values
+        data:data
       })
     .then((res) => {
         if(res && res.status === 200){
-        this.setState({
-            data: res.data.result,
-            nowCurrent:res.data.result.pageNum,
-            status:status,
-            // roleCode:roleCode,
-        });
-        console.log(res.data)
+          console.log(res.data)
+          this.getInfo(FIRST_PAGE)
         }
     })
     .catch(function (error) {
         console.log(error);
     });
-      
-}
+  }
   //返回不同的状态按钮
 
   render(){
@@ -199,7 +256,25 @@ class Inspection extends Component{
                  <Link
                   to={`/cbd/item/detail/${record.id}`}
                   style={{marginRight:'12px'}}
-                >详情</Link>          
+                >详情</Link> 
+                <Popconfirm
+                    title="确定同意该巡检任务的执行？"
+                    onConfirm={()=> {this.approveImcTask(record.id)}}
+                >
+                    <Button 
+                    type="simple"
+                    style={{marginRight:'12px',border:'none',padding:0,color:"#357aff",background:'transparent',display:this.state.display_button1}}
+                    >同意执行</Button>
+                </Popconfirm>      
+                <Popconfirm
+                    title="确定接单？"
+                    onConfirm={()=> {alert("服务商接单啦")}}
+                >
+                    <Button 
+                    type="simple"
+                    style={{marginRight:'12px',border:'none',padding:0,color:"#357aff",background:'transparent',display:this.state.display_button2}}
+                    >接单</Button>
+                </Popconfirm>  
               </div>
             ),
           }]}
